b8306a5b54c9b74d2a8ae3323d9f7f5ab97ea28c
pitiwat work <pitiwat.mike.work@gmail.com>
2025-08-22 15:47:37 +0700

    feat: init setting style

---
+++ src/components/common/theme-toggle.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="fixed top-4 right-4 p-3 border rounded-sm"
+    >
+      {theme === "dark" ? "🌞" : "🌙"}
+    </button>
+  );
+}
